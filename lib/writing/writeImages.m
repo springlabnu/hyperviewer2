33256953aceaf87b1fe620c1f055efdf4b5f0b97
Ryan Lang <rlang81@gmail.com>
2021-03-23 16:43:40 -0400

    Updating image saving functionality for hyperim objects

--- lib/writing/writeImages.m
+++ lib/writing/writeImages.m
@@ -1,32 +1,46 @@
-function writeImages(im, save_dir, handles)
-
+function writeImages(im, save_dir, app)
+%% Saves im (hyperim class) in directory dir, needs app object from Hyperviewer2
 % Write colored composite raw-image
 wlfilt = [im.filt];
-imwrite( sum(cat(4, im.colorims.cube(wlfilt).im), 4), ... / sum(wlfilt), ...
-         [save_dir 'raw_composite_image.tif'] );
+rawcomp = im.colorImage(app, wlfilt, 'cube');
+imwrite(rawcomp,[save_dir 'raw_composite_image.tif'] );
 
 % Write colored composite basis-image
-specfilt = [handles.spec.filt];
-try
-imwrite( sum(cat(4, im.colorims.basis(specfilt).im), 4), ...
-         [save_dir 'basis_composite_image.tif'] );
-catch
-    imwrite( sum(cat(4, im.colorims.basis.im), 4), ...
-         [save_dir 'basis_composite_image.tif'] );
+specfilt = 1:app.cfg.num_spec;
+specfilt = specfilt(logical([app.spec.filt])); % In case filt was not a bool
+colorcomp = im.colorImage(app, specfilt, 'x');
+imwrite(colorcomp, [save_dir 'basis_composite_image.tif']);
+
+% Single channel images
+for i = 1:app.cfg.num_spec
+    % Save Color Images
+    colorbasis = im.colorImage(app, i, 'x');
+    imwrite(colorbasis, [save_dir 'basis_image_' ...
+        app.spec(i).label '.tif'] );
+    
+    % Save Raw basis maps as 16-bit tiffs
+    imwrite( uint16(im.x(:,:,i) * 2^16), [save_dir 'basis_image_intensity_map_' ...
+        app.spec(i).name '.tif'] );
 end
-% % Write the saturated pixel mask image
-% imwrite( uint16(L_sat_all .* 2^16), ...
-%     [save_dir 'saturated pixel mask' '.tif'] );
+
+%% Optional additional files
+
+% Save spectral data in roi as .mat file
+if false
+    save([save_dir 'analysis_data.mat'], 'im');
+end
 
 % Write chi-squared map
-f = figure('visible', 'off');
-imagesc(im.redchisq);
-axis square
-set(gca, 'XTickLabel', [], 'YTickLabel', []);
-set(f, 'Color', 'none');
-colorbar; colormap jet;
-export_fig(f, [save_dir 'ChiSquaredMap.png'], '-native');
-clear f;
+if false
+    f = figure('visible', 'off');
+    imagesc(im.redchisq);
+    axis square
+    set(gca, 'XTickLabel', [], 'YTickLabel', []);
+    set(f, 'Color', 'none');
+    colorbar; colormap jet;
+    export_fig(f, [save_dir 'ChiSquaredMap.png'], '-native');
+    clear f;
+end
 
 % Calculate diff between CPU & GPU
 % if handles.gpuUnmix
@@ -58,30 +72,12 @@ if false
 %     
 end
 
-% Save spectral data in roi
-if true
-    save([save_dir 'analysis_data.mat'], 'im');
-end
-
-for i = 1:handles.cfg.num_spec
-    
-    % Save Color Images
-    imwrite( im.colorims.basis(i).im * handles.state.brightness, ...
-        [save_dir 'basis_image_' ...
-        handles.spec(i).label '.tif'] );
-    
-    % Save Raw basis maps as 16-bit tiffs
-    imwrite( uint16(im.x(:,:,i) * 2^16), ...
-        [save_dir 'basis_image_intensity_map_' ...
-        handles.spec(i).name '.tif'] );
-
-end
-
+% Generate tiled image
 if false
-    % Generate tiled image
-    tiles = makeTiledImage(im, handles);
     
-    parts = strsplit(handles.expt.dirname, filesep);
+    tiles = makeTiledImage(im, app);
+    
+    parts = strsplit(app.expt.dirname, filesep);
     
     % Save image
     imwrite(tiles, [save_dir 'tiled_basis_images_' im.label '_' parts{end} '.tif']);
