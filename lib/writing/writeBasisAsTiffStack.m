7a9e0c44e6dda41926eeeacd23302ed23b408449
Ryan Lang <rlang81@gmail.com>
2021-04-01 10:40:32 -0400

    Fixes to Zeiss images

--- lib/writing/writeBasisAsTiffStack.m
+++ lib/writing/writeBasisAsTiffStack.m
@@ -1,10 +1,9 @@
-function writeBasisAsTiffStack(im, save_dir, subdirs, filename, app)
+function writeBasisAsTiffStack(im, save_dir, subdirs, app)
 % WRITEBASISASTIFFSTACK   Tiff stack save function for HyperViewer2
 %   writeBasisAsTiffStack(im, save_dir, subdirs, filename, app) Saves unmixed images to a Tiff stack
 %       im              hyperim class object, unmixed by HyperViewer2
 %       save_dir        directory to save image files
 %       subdirs         1x3 cell of directory names for composite, individual color and individal grayscale image folders
-%       filename        name of image to output - needs to be unique, overwriting is not checked
 %       app             HyperViewer2 app instance
 
 % Write colored composite basis-image
@@ -12,12 +11,12 @@ function writeBasisAsTiffStack(im, save_dir, subdirs, filename, app)
 specfilt = 1:app.cfg.num_spec;
 specfilt = specfilt(logical([app.spec.filt])); % In case filt was not a bool
 colorcomp = im.colorImage(app, specfilt, 'x');
-imwrite(colorcomp, [save_dir subdirs{1} filename 'basis_composite_image.tif']);
+imwrite(colorcomp, [save_dir subdirs{1} im.filename 'basis_composite_image.tif']);
 
 % Set up tiff objects for writing stacks
 
-tColor = Tiff([save_dir subdirs{2} filename 'basis_stack_color.tif'],'w');
-tBW = Tiff([save_dir subdirs{3} filename 'basis_stack_intensity_map.tif'],'w');
+tColor = Tiff([save_dir subdirs{2} im.filename 'basis_stack_color.tif'],'w');
+tBW = Tiff([save_dir subdirs{3} im.filename 'basis_stack_intensity_map.tif'],'w');
 
 tagstructC.ImageLength = size(colorcomp, 1);
 tagstructC.ImageWidth = size(colorcomp, 2);
@@ -47,8 +46,8 @@ for i = 1:app.cfg.num_spec
     tColor.setTag(tagstructC);
     tBW.setTag(tagstructBW);
     write(tColor, uint16(im.colorImage(app, i, 'x') * (2^16-1)));
-    write(tBW, uint16(im.x(:,:,i) * (2^16-1))), 
-    if i ~= length(im.colorims.basis)
+    write(tBW, uint16(im.x(:,:,i) * (2^16-1)));
+    if i ~= app.cfg.num_spec
        tColor.writeDirectory();
        tBW.writeDirectory();
     end  
